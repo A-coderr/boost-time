1c8edce39757ef578153f0d14a30dc9edfd4c746
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2025-11-04 12:21:56 -0500

    Movement Addition
    Added the thrust force to the rocket player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,25 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] float thrustForce = 10000f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
+        }
     }
 }
