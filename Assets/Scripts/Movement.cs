3aca8c648fab902ab681154f7610883f83db7216
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2025-11-05 10:19:14 -0500

    Added Gravity

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
     [SerializeField] InputAction thrust;
     [SerializeField] InputAction rotation;
     [SerializeField] float thrustForce = 1000f;
+    [SerializeField] float rotationForce = 100f;
 
     Rigidbody rb;
 
@@ -39,11 +40,18 @@ public class Movement : MonoBehaviour
         float rotationInput = rotation.ReadValue<float>();
         if (rotationInput < 0)
         {
-            transform.Rotate(Vector3.forward);
+            ApplyRotation(rotationForce);
         }
-        if (rotationInput > 0)
+        else if (rotationInput > 0)
         {
-            transform.Rotate(-Vector3.forward);
+            ApplyRotation(-rotationForce);
         }
+    }
+
+    private void ApplyRotation(float rotationFrame)
+    {
+        rb.freezeRotation = true; // Temporarily freeze physics rotation
+        transform.Rotate(Vector3.forward * rotationFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false; // Unfreeze physics rotation
     }
 }
